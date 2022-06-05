cf711365cf6a1f18b1ae1bc044a8d18e1c4bef2d
flowerweb1966 <102278033+flowerweb1966@users.noreply.github.com>
2022-06-05 23:11:50 +0000

    Update _workk.js

--- _workk.js
+++ _workk.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'vvytxxuiyhha.herokuapp.com'
+      url.hostname = 'xxessurehd2.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
